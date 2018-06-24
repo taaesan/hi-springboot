214e7df108505be9c479e3bc47b5aee38b023f43
taaesan <taaesan@admins-iMac.local>
2018-06-24 13:03:01 +0700

    Add Controller

--- src/main/java/com/taae/examples/hispringboot/HiSpringbootApplication.java
+++ src/main/java/com/taae/examples/hispringboot/HiSpringbootApplication.java
@@ -2,11 +2,27 @@ package com.taae.examples.hispringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HiSpringbootApplication {
+public class HiSpringbootApplication extends SpringBootServletInitializer {
 
+	/**
+	 * Used when run as a jar
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(HiSpringbootApplication.class, args);
 	}
+
+	/**
+	 * Used when run war
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HiSpringbootApplication.class);
+	}
+
+
 }
